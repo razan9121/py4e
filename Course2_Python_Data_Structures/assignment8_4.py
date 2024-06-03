4edc42d11c26b59aadfeefcfe865c8e5f12617e0
razan9121 <razan@Desctop>
2024-06-03 06:33:15 +0300

    Add assignments 8.4 and 8.5

---
+++ Course2_Python_Data_Structures/assignment8_4.py
@@ -0,0 +1,18 @@
+fname = input('Enter file name: ')
+try:
+    fh = open(fname)
+except:
+    print('Invalid file name')
+    quit()
+    
+lst = list()
+for line in fh:
+    line = line.rstrip()  
+    words = line.split()    
+
+    for w in words:
+        if w not in lst:
+            lst.append(w)
+
+lst.sort()
+print(lst)
---
+++ Course2_Python_Data_Structures/assignment8_5.py
@@ -0,0 +1,19 @@
+fname = input("Enter file name: ")
+try:
+    fh = open(fname)
+except:
+    print('Invalid input')
+    quit()
+
+count = 0
+for line in fh:
+    line = line.rstrip()
+
+    if not line.startswith('From '):
+        continue
+    words = line.split()
+    sw = words[1]
+    
+    count = count + 1
+    print(sw)
+print('There were', count, 'lines in the file with From as the first word')
